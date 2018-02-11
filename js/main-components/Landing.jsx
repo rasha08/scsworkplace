3bc9ed763e72d4218ae875a3eaa470814da0a765
rasha08 <rasha08@gmail.com>
2018-02-11 20:59:39 +0100

    small style changes

--- js/main-components/Landing.jsx
+++ js/main-components/Landing.jsx
@@ -4,7 +4,7 @@ import UserProjects from './UserProjects';
 import LinkWrapper from '../helpers/LinkWrapper';
 
 const NoProjectLanding = () => (
-  <div>
+  <div className="no-project">
     <p className="lead">
       You are currently not assigned to any poject, you can create a new project if you are par of existing WORKPLACE or create a Workplace and organize your team!
       {' '}
@@ -29,21 +29,21 @@ const Landing = props => (
     <div id="content">
       <h1 className="cover-heading">
         {!isEmpty(props.projects)
-          ? `Welcome ${get(props.user, 'displayName')} - your pojects list:`
+          ? <small>Hello {get(props.user, "displayName")},  check out your pojects list:</small>
           : 'Welcome to Smart Cat Solutions Workplace'}
       </h1>
       {!isEmpty(props.projects)
         ? <div>
             <LinkWrapper
               to="/create-project"
-              className="btn btn-lg btn-dark"
+              className="btn btn-sm btn-dark"
               id="create-project"
             >
               Create Project
             </LinkWrapper>
             <LinkWrapper
               to="/create-new-workplace"
-              className="btn btn-lg btn-secondary"
+              className="btn btn-sm btn-secondary"
               id="create-workplace"
             >
               Create Workplace
