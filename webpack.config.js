0c1884c6e96cbc9bb2b0c6e2f8d18b1d0cf064ef
rasha08 <rasha08@gmail.com>
2018-03-09 08:15:34 +0100

    speed optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,55 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	context: __dirname,
-	entry:[
-	//	"react-hot-loader/patch",
-		//"webpack-dev-server/client?http://localhost:8080",
-	//	"webpack/hot/only-dev-server",
-		"./js/ClientApp.jsx"
-		],
-	devtool: "cheap-eval-source-map",
-	output:{
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js',
-		publicPath: '/public/'
-	},
-	devServer:{
-		hot: false,
-		publicPath: '/public/',
-		historyApiFallback: true
-	},
-	resolve:{
-		extensions: ['.js', '.jsx', '.json']
-	},
-	stats:{
-		color:true,
-		reasons: true,
-		chunks: false
-	},
-	plugins:[
-		//new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin()
-	],
-	module:{
-		rules:[
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader'
-			}
-		]
-	}
-}
+  context: __dirname,
+  entry: [
+    //	"react-hot-loader/patch",
+    //"webpack-dev-server/client?http://localhost:8080",
+    //	"webpack/hot/only-dev-server",
+    './js/ClientApp.jsx'
+  ],
+  // devtool: 'eval',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    hot: false,
+    publicPath: '/public/',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    color: true,
+    reasons: true,
+    chunks: false
+  },
+  plugins: [
+    //new webpack.HotModuleReplacementPlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
