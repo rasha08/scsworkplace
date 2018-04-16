175826484684f6d2dd983a964b2fec526fcd953e
rasha08 <rasha08@gmail.com>
2018-04-16 21:29:49 +0200

    fixed bug with cardd footer

--- js/helpers/form-field-helpers/FormTextAreaField.jsx
+++ js/helpers/form-field-helpers/FormTextAreaField.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const FormTextAreaField = props => {
+  const getTextAreaNumberOfRows = value => {
+    if (!value) {
+      return false;
+    }
+
+    let defaultNumOfRows = props.value.length / 90;
+
+    return defaultNumOfRows < 3 ? 3 : defaultNumOfRows;
+  };
+
   return (
     <div className="form-group">
       <label>{`${props.type} ${props.fieldName}`}</label>
       <textarea
+        rows={getTextAreaNumberOfRows(props.value)}
         className="form-control"
         id={props.filedName}
         placeholder={`${props.type} ${props.fieldName}`}
--- js/helpers/task-components/TaskActions.jsx
+++ js/helpers/task-components/TaskActions.jsx
@@ -1,43 +1,91 @@
 import React from 'react';
+import TaskTooltip from './taskActionTooltip';
 
-const TaskActions = (props) => {
+const TaskActions = props => {
   const changeTaskColumn = (event, direction) => {
     let index;
     if (direction === 'left' && props.index !== 0) {
       index = props.index - 1;
-      props.utils.changeTaskColumn(event, props.projectNameSlug, props.task, index);
+      props.utils.changeTaskColumn(
+        event,
+        props.projectNameSlug,
+        props.task,
+        index
+      );
     } else if (direction === 'right' && props.index !== props.lastIndex) {
       index = props.index + 1;
       let isDone = index === props.lastIndex;
-      props.utils.changeTaskColumn(event, props.projectNameSlug, props.task, index, isDone);
+      props.utils.changeTaskColumn(
+        event,
+        props.projectNameSlug,
+        props.task,
+        index,
+        isDone
+      );
     }
-  }
+  };
 
   const assignUser = event => {
     props.utils.assignUserToTask(event, props.projectNameSlug, props.task);
-  }
+  };
 
   const assignReviewerToTask = event => {
     props.utils.assignReviewerToTask(event, props.projectNameSlug, props.task);
-  }
+  };
 
   const blockTask = event => {
     props.utils.blockTask(event, props.projectNameSlug, props.task);
-  }
+  };
+
+  const absorbClick = event => {
+    event.preventDefault();
+    return false;
+  };
 
   return (
-    <div className="card-body bottom">
-        <i className={props.index === 0 ? "material-icons disabled" : "material-icons"}
-          onClick={(event) => changeTaskColumn(event, 'left')}>assignment_return</i>
-        <i className={props.task.assigner ? "material-icons assigned" : "material-icons"}
-          onClick={event => assignUser(event)}>assignment_ind</i>
-        <i className="material-icons" onClick={event => blockTask(event)}>block</i>
-        <i className={props.task.reviewer ? "material-icons assigned" : "material-icons"}
-          onClick={event => assignReviewerToTask(event)}>assignment_turned_in</i>
-        <i className={props.task.state === 'blocked' ? "material-icons disabled" : "material-icons"}
-          onClick={(event) => changeTaskColumn(event, 'right')}>forward</i>
+    <div>
+      <div className="card-body bottom" onClick={$event => absorbClick($event)}>
+        <i
+          className={
+            props.index === 0 ? 'material-icons disabled' : 'material-icons'
+          }
+          onClick={event => changeTaskColumn(event, 'left')}
+        >
+          assignment_return
+        </i>
+        <i
+          className={
+            props.task.assigner ? 'material-icons assigned' : 'material-icons'
+          }
+          onClick={event => assignUser(event)}
+        >
+          assignment_ind
+        </i>
+        <i className="material-icons" onClick={event => blockTask(event)}>
+          block
+        </i>
+        <i
+          className={
+            props.task.reviewer ? 'material-icons assigned' : 'material-icons'
+          }
+          onClick={event => assignReviewerToTask(event)}
+        >
+          assignment_turned_in
+        </i>
+        <i
+          className={
+            props.task.state === 'blocked'
+              ? 'material-icons disabled'
+              : 'material-icons'
+          }
+          onClick={event => changeTaskColumn(event, 'right')}
+        >
+          forward
+        </i>
+      </div>
+      {/* <TaskTooltip value={tooltipedElement} /> */}
     </div>
-  )
-}
+  );
+};
 
 export default TaskActions;
